5a3976ec01fe085d45624057524978821cf11f2a
Sonya256895 <84439863+Sonya256895@users.noreply.github.com>
2022-06-11 08:45:49 +0200

    Добавьте файлы проекта.

---
+++ Progect1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Progect1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "мяу";
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+    }
+}
